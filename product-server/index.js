ad61a3befafd5cdbd72ad20bb3843714ed5bcdfb
username <nicholashanr@gmail.com>
2021-08-13 15:01:40 -0700

    implement product info requests

--- product-server/index.js
+++ product-server/index.js
@@ -13,31 +13,57 @@ pool.connect((err) => {
   }
 })
 
-const productQuery = () => {
+const featuresQuery = (id) => {
   return new Promise ((resolve, reject) => {
-    pool.query(`SELECT * FROM product`, [], (err, res) => {
+    pool.query(`SELECT * FROM features WHERE product_id = ${id}`, [], (err, res) => {
       if (err) {
         reject(err);
       } else {
-        resolve(res.rows[0])
+        resolve(res.rows);
       }
     })
   })
 }
 
+const productQuery = (id) => {
+  return new Promise ((resolve, reject) => {
+    pool.query(`SELECT * FROM product WHERE id = ${id}`, [], (err, res) => {
+      if (err) {
+        reject(err);
+      } else {
+        let infoObj = res.rows[0];
+        featuresQuery(id)
+          .then(features => {
+            infoObj.features = [];
+            features.forEach(feature => {
+              infoObj.features.push({
+                feature: feature.feature,
+                value: feature.value
+              })
+            })
+            resolve(infoObj)
+          })
+      }
+    })
+  })
+}
+
+
 //ROUTES
 
-//get all products
-app.get('/products', (req, res) => {
-  productQuery()
+//get product info by id
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  productQuery(id)
     .then(products => {
-      console.log(products);
       res.send(products);
     })
     .catch(err => {
       console.log(err);
     })
 })
+
+
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
